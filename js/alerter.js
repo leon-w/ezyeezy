fa7be7c269c6675bb111b0aff967d116666aaba1
Leon Wiemers <leonwiemers@gmail.com>
2019-09-21 20:07:11 +0200

    fix sound

--- js/alerter.js
+++ js/alerter.js
@@ -1,8 +1,10 @@
+let alertSound = new Audio(chrome.runtime.getURL("audio/alert.mp3"));
+
 fetch(chrome.runtime.getURL("config.json")).then(response => response.json().then(config => {
     setTimeout(() => {
         if ($(`*:contains(${config.keyword})`).length) {
             console.log("FOUND KEYWORD");
-            new Audio(chrome.runtime.getURL("audio/alert.mp3")).play();
+            alertSound.play();
             let toggle = 0;
             setInterval(() => {
                 document.title = ["!!!!!!!!!!!!!!!!!!!!!!!!!!!", "DROP DROP DROP DROP"][toggle];
