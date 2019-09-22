568d86b8e2ae618e4d35106fb2f8dde05f7b5a11
leon-w <leonwiemers@gmail.com>
2019-09-22 21:01:02 +0200

    Create README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# EZ YEEZY
+
+simple chrome extension to help detect drops at [adiadas.de/yeezy](https://adidas.de/yeezy)
+
+### installation
+
+* download source, extract the zip
+* goto `chrome://extensions`
+* enable developer mode
+* click `Load unpacked` and select the extracted folder
--- settings/settings.js
+++ settings/settings.js
@@ -1,11 +1,13 @@
 function save_options() {
     let enable = document.getElementById("cb_enable").checked;
     let refresh_delay = document.getElementById("tb_refresh_delay").value;
+    let load_time = document.getElementById("tb_load_time").value;
     let keyword = document.getElementById("tb_keyword").value;
     let loop = document.getElementById("cb_loop").checked;
     chrome.storage.sync.set({
         enable,
         refresh_delay,
+        load_time,
         keyword,
         loop
     }, () => {
@@ -22,12 +24,14 @@ function save_options() {
 function restore_options() {
     chrome.storage.sync.get({
         refresh_delay: 30000,
+        load_time: 3000,
         keyword: "warte",
         loop: false,
         enable: true
     }, data => {
         document.getElementById("cb_enable").checked = data.enable;
         document.getElementById("tb_refresh_delay").value = data.refresh_delay;
+        document.getElementById("tb_load_time").value = data.load_time;
         document.getElementById("tb_keyword").value = data.keyword;
         document.getElementById("cb_loop").checked = data.loop;
     });
